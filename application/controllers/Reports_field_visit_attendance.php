c0ff04c6eaf63cb322d557544e57bc7806b1ee38
Saiful Islam <saifultitu90@gmail.com>
2018-05-12 12:00:45 +0600

    Reports field visit attendance updated

--- application/controllers/Reports_field_visit_attendance.php
+++ application/controllers/Reports_field_visit_attendance.php
@@ -70,6 +70,8 @@ class Reports_field_visit_attendance extends Root_Controller
                     }
                 }
             }
+
+            $this->db->where('user_area.territory_id >',0);
             $this->db->where('user_area.revision',1);
             $this->db->where('user.status',$this->config->item('system_status_active'));
             $this->db->where('user_info.revision',1);
@@ -273,6 +275,7 @@ class Reports_field_visit_attendance extends Root_Controller
                     }
                 }
             }
+            $this->db->where('user_area.territory_id >',0);
         }
         if($user_id)
         {
