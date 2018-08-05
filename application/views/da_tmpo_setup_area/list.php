a7e8c18bc03a25f47105107f025d9110e7930509
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2018-08-05 09:07:56 +0600

    rename view folder growing area setup

---
+++ application/views/da_tmpo_setup_area/list.php
@@ -0,0 +1,120 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$CI=& get_instance();
+$action_buttons=array();
+if(isset($CI->permissions['action2']) && ($CI->permissions['action2']==1))
+{
+    $action_buttons[]=array
+    (
+        'type'=>'button',
+        'label'=>'Area Setup',
+        'class'=>'button_jqx_action',
+        'data-action-link'=>site_url($CI->controller_url.'/index/area_list')
+    );
+}
+if(isset($CI->permissions['action4']) && ($CI->permissions['action4']==1))
+{
+    $action_buttons[]=array(
+        'type'=>'button',
+        'label'=>$CI->lang->line("ACTION_PRINT"),
+        'class'=>'button_action_download',
+        'data-title'=>"Print",
+        'data-print'=>true
+    );
+}
+if(isset($CI->permissions['action5']) && ($CI->permissions['action5']==1))
+{
+    $action_buttons[]=array(
+        'type'=>'button',
+        'label'=>$CI->lang->line("ACTION_DOWNLOAD"),
+        'class'=>'button_action_download',
+        'data-title'=>"Download"
+    );
+}
+if(isset($CI->permissions['action6']) && ($CI->permissions['action6']==1))
+{
+    $action_buttons[]=array
+    (
+        'label'=>'Preference',
+        'href'=>site_url($CI->controller_url.'/index/set_preference')
+    );
+}
+$action_buttons[]=array(
+    'label'=>$CI->lang->line("ACTION_REFRESH"),
+    'href'=>site_url($CI->controller_url.'/index/list')
+);
+$CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
+
+?>
+
+<div class="row widget">
+    <div class="widget-header">
+        <div class="title">
+            <?php echo $title; ?>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <?php
+    if(isset($CI->permissions['action6']) && ($CI->permissions['action6']==1))
+    {
+        $CI->load->view('preference',array('system_preference_items'=>$system_preference_items));
+    }
+    ?>
+    <div class="col-xs-12" id="system_jqx_container">
+
+    </div>
+</div>
+<div class="clearfix"></div>
+<script type="text/javascript">
+    $(document).ready(function ()
+    {
+        system_preset({controller:'<?php echo $CI->router->class; ?>'});
+        var url = "<?php echo site_url($CI->controller_url.'/index/get_items');?>";
+
+        var source =
+        {
+            dataType: "json",
+            dataFields: [
+                { name: 'id', type: 'int' },
+                <?php
+                foreach($system_preference_items as $key => $value){ ?>
+                { name: '<?php echo $key; ?>', type: 'string' },
+                <?php } ?>
+            ],
+            id: 'id',
+            type: 'POST',
+            url: url
+        };
+
+        var tooltiprenderer = function (element) {
+            $(element).jqxTooltip({position: 'mouse', content: $(element).text() });
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source);
+        // create jqxgrid.
+        $("#system_jqx_container").jqxGrid(
+            {
+                width: '100%',
+                source: dataAdapter,
+                pageable: true,
+                filterable: true,
+                sortable: true,
+                showfilterrow: true,
+                columnsresize: true,
+                pagesize:50,
+                pagesizeoptions: ['20', '50', '100', '200','300','500'],
+                selectionmode: 'singlerow',
+                altrows: true,
+                height: '350px',
+                enablebrowserselection:true,
+                columnsreorder: true,
+                columns:[
+                    { text: '<?php echo $CI->lang->line('LABEL_OUTLET_NAME'); ?>',pinned:true,dataField: 'outlet',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['outlet']?0:1;?>},
+                    { text: '<?php echo $CI->lang->line('LABEL_DIVISION_NAME'); ?>',dataField: 'division_name',width:'180',filtertype: 'list',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['division_name']?0:1;?>},
+                    { text: '<?php echo $CI->lang->line('LABEL_ZONE_NAME'); ?>',dataField: 'zone_name',width:'180',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['zone_name']?0:1;?>},
+                    { text: '<?php echo $CI->lang->line('LABEL_TERRITORY_NAME'); ?>',dataField: 'territory_name',width:'180',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['territory_name']?0:1;?>},
+                    { text: '<?php echo $CI->lang->line('LABEL_DISTRICT_NAME'); ?>',dataField: 'district_name',width:'180',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['district_name']?0:1;?>},
+                    { text: '<?php echo $CI->lang->line('LABEL_NUMBER_OF_AREAS'); ?>',dataField: 'number_of_areas',width:'100',cellsalign: 'right',rendered:tooltiprenderer,hidden: <?php echo $system_preference_items['number_of_areas']?0:1;?>}
+                ]
+            });
+    });
+</script>
