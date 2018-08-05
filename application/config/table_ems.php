5dd34922c3018dae769cae520cb8256c38a39ae9
Saiful Islam <saifultitu90@gmail.com>
2018-08-05 12:35:36 +0600

    Lead Farmer Setup Task

--- application/config/table_ems.php
+++ application/config/table_ems.php
@@ -34,3 +34,4 @@ $config['table_ems_ft_field_visit_disease_picture'] = 'arm_ems_2018_19.ems_ft_fi
 
 //Daily Activities
 $config['table_ems_da_tmpo_setup_areas'] = 'arm_ems_2018_19.ems_da_tmpo_setup_areas';
+$config['table_ems_da_tmpo_setup_area_lead_farmers'] = 'arm_ems_2018_19.ems_da_tmpo_setup_area_lead_farmers';
--- application/language/english/daily_activities_lang.php
+++ application/language/english/daily_activities_lang.php
@@ -1,2 +1,4 @@
 <?php
 $lang['LABEL_NUMBER_OF_AREAS'] = 'Number Of Areas';
+$lang['LABEL_NUMBER_OF_LEAD_FARMERS'] = 'Number Of Lead Farmers';
+$lang['LABEL_AREA_NAME'] = 'Area Name';
