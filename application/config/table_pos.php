0cf03310c310f28c949d96b49c2e431b76eb716f
Md. Shaiful Islam <islambuet@gmail.com>
2018-10-13 01:35:20 +0600

    ems common config included

--- application/config/table_pos.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['table_pos_setup_farmer_farmer'] = 'arm_beeztola_2018_19.pos_setup_farmer_farmer';
-$config['table_pos_setup_farmer_outlet'] = 'arm_beeztola_2018_19.pos_setup_farmer_outlet';
