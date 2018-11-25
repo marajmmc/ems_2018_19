cd4cb069c687e8980e13ea9548e0e1a9994bb262
Md. Shaiful Islam <islambuet@gmail.com>
2018-11-26 04:23:52 +0600

    Removed Transfer controller and test controller Fixed

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,50 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
     public function index()
     {
-        $this->load->library('user_agent');
-
-        /*if ($this->agent->is_browser())
-        {
-            $agent = $this->agent->browser().' '.$this->agent->version();
-        }
-        elseif ($this->agent->is_robot())
-        {
-            $agent = $this->agent->robot();
-        }
-        elseif ($this->agent->is_mobile())
-        {
-            $agent = $this->agent->mobile();
-        }
-        else
-        {
-            $agent = 'Unidentified User Agent';
-        }*/
-        echo '<pre>';
-        print_r($this->agent);
-        print_r($_SERVER);
-        echo '</pre>';
-
-        echo '</br>';
-        $ip = $this->input->ip_address();
-        echo $ip;
-        echo $this->agent->platform(); // Platform info (Windows, Linux, Mac, etc.)
 
     }
 
